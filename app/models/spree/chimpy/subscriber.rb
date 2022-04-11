b5ca399ffbd15eae0d7e0accfc4e91618f6a7d0d
umairrazam <omairrr.azam@gmail.com>
2022-04-11 23:38:41 +0500

    fix(issues): subcriber

--- app/models/spree/chimpy/subscriber.rb
+++ app/models/spree/chimpy/subscriber.rb
@@ -10,10 +10,17 @@ class Spree::Chimpy::Subscriber < ActiveRecord::Base
   around_update :resubscribe
   after_destroy :unsubscribe
 
-  delegate :subscribe, :resubscribe, :unsubscribe, to: :subscription
+  # delegate :subscribe, :resubscribe, :unsubscribe, to: :subscription
 
 private
   def subscription
     Spree::Chimpy::Subscription.new(self)
   end
+
+  def subscribe
+    gibbon = Gibbon::Request.new(api_key: "97ff800b55a5527cb38ce3102464e4bb-us2")
+    gibbon.timeout = 10 
+    gibbon.lists("5dbf468fc5").members.create(body: { email_address: self.email, status: "subscribed", merge_fields: {}})
+
+  end  
 end
